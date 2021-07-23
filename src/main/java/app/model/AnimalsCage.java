3cbc1fc8d1c69982b4c73fba3c650a295b5da995
JIexa <msleha100@gmail.com>
2021-07-23 14:53:08 +0300

    добавил getTimer()

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -14,15 +14,15 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
+    public Timer getTimer() {
+        return timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(timer.getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
-    }
-
-    public Timer getTimer() {
-        return timer;
     }
 }
